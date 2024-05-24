5898ab567f5d8cb3f8005a4211fd3847c4085191
Dunderz <isaacknam711@gmail.com>
2024-05-24 16:22:18 -0400

    docker practice

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 200,
+    },
+  }),
+};
 
 export default nextConfig;
